eaa80e185ef1c13f0395514373534037b7ebaa4d
KenNyagz <kennyagz681@gmail.com>
2024-06-07 01:52:49 +0300

    lockboxes challenge

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+An array of arrays, each entry is a box with a key to another. Box no is same
+key no.as A solution that can efficiently determine if all boxes can be opened
+'''
+
+
+def canUnlockAll(boxes):
+    ''' Iterative approach'''
+    n = len(boxes)
+    unlocked = [False] * n  # Track which boxes are unlocked
+    stack = 0  # Start with the initial box
+
+    while stack:
+        current = stack.pop()
+        if not unlocked[current]:
+            unlocked[current] = True  # Mark the current box as unlocked
+            key = boxes[current]  # Get the key inside the current box
+            if not unlocked[key]:  # If the box to which the key belongs is not unlocked
+                stack.append(key)  # Add it to the stack
+
+    return all(unlocked)
