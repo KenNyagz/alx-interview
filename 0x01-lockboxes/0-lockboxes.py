0d9a3c2f0f6594f7b5cd89a0cc3e5ce683dc8d2f
KenNyagz <kennyagz681@gmail.com>
2024-06-16 17:27:58 +0300

    logboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,6 +4,7 @@ An array of arrays, each entry is a box with a key to another. Box no is same
 key no.as A solution that can efficiently determine if all boxes can be opened
 '''
 
+
 def canUnlockAll(boxes):
     unlocked = [0]
     for box_id, box in enumerate(boxes):
