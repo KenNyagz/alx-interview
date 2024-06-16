c4af3fecfbdcee7be3a22701681f0a47cf47fb95
KenNyagz <kennyagz681@gmail.com>
2024-06-16 17:01:13 +0300

    logboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,18 +5,23 @@ key no.as A solution that can efficiently determine if all boxes can be opened
 '''
 
 
-def canUnlockAll(boxes, start=[]):
-    ''' Iterative approach'''
-    n = len(boxes)
-    unlocked = [False] * n  # Track which boxes are unlocked
-    stack = [start]  # Start with the initial box
+def join(T,R):
+  res =[]
+  for e in R:
+    res += T[e]
+  return res
 
-    while stack:
-        current = stack.pop()
-        if not unlocked[current]:
-            unlocked[current] = True  # Mark the current box as unlocked
-            key = boxes[current]  # Get the key inside the current box
-            if not unlocked[key]:  # If the box to which the key belongs is not unlocked
-                stack.append(key)  # Add it to the stack
-
-    return all(unlocked)
+def canUnlockAll(boxes):
+  index = 0
+  total = list(set(boxes[0])| {0})
+  added = True
+  while added:
+    added = False
+    for j in join(boxes,total[index:]):
+      if j not in total:
+        total.append(j)
+        index +=1
+        added= True
+  #print(total)
+  
+  return len(total)==len(boxes)
