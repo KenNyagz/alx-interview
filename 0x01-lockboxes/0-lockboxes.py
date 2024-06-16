10ed2a3ad62f4c39c3e5a27e907d3206fba0847c
KenNyagz <kennyagz681@gmail.com>
2024-06-16 17:08:20 +0300

    logboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,23 +5,24 @@ key no.as A solution that can efficiently determine if all boxes can be opened
 '''
 
 
-def join(T,R):
-  res =[]
-  for e in R:
-    res += T[e]
-  return res
+def join(T, R):
+    res = []
+    for e in R:
+        res += T[e]
+    return res
+
 
 def canUnlockAll(boxes):
-  index = 0
-  total = list(set(boxes[0])| {0})
-  added = True
-  while added:
-    added = False
-    for j in join(boxes,total[index:]):
-      if j not in total:
-        total.append(j)
-        index +=1
-        added= True
-  #print(total)
-  
-  return len(total)==len(boxes)
+    index = 0
+    total = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes, total[index:]):
+            if j not in total:
+                total.append(j)
+                index += 1
+                added = True
+    # print(total)
+
+    return len(total) == len(boxes)
